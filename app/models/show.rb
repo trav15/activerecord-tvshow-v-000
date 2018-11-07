ae391b9591caa0271f7027644391aea74b75f835
Travis T <travistokuyama@gmail.com>
2018-11-07 19:01:22 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,6 +1,6 @@
 class Show < ActiveRecord::Base
   def self.highest_rating
-    Show.maximum(:rating)
+    self.maximum(:rating)
   end
 
   def self.most_popular_show
@@ -8,10 +8,10 @@ class Show < ActiveRecord::Base
   end
 
   def self.lowest_rating
-    Show.minimum(:rating)
+    self.minimum(:rating)
   end
 
   def self.least_popular_show
-    Show.where(Show.minimum(:rating)).first
+    self.where("rating = ?", self.lowest_rating).first
   end
 end
